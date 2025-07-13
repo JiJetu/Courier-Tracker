525f3848490665a543c70320da552c1e46980639
Md Jaoadul Islam <jijetu2@gmail.com>
2025-07-13 22:53:33 +0600

    redux set-up complated

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </HelmetProvider>
   </React.StrictMode>
 );
